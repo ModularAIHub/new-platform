2a6b9a00671dc4a6616b697e8bcd1f77254c80e2
kanishk2404 <saraswatkanishk24@gmail.com>
2026-03-03 02:04:04 +0530

    chore: remove temporary seo and parser scripts

--- client/test-parser.js
+++
@@ -1,4 +0,0 @@
-import { markdownToHTML } from './src/utils/blogHelpers.js';
-const markdown = 'Here is what [Buffer, Hootsuite, and the other giants](/blogs/comparisons/suitegenie-vs-hootsuite) are doing';
-const html = markdownToHTML(markdown);
-console.log(html);
